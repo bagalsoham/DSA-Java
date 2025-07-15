5437bc411538bec64c177cfff6e4da93dc3c3b04
Soham <bagalsoham1717@gmail.com>
2025-07-15 19:15:55 +0530

    Day 1

---
+++ ReverseArray.java
@@ -0,0 +1,17 @@
+package DSA;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        for(int i = 0; i < n/2; i++){
+            //swap the elements between array [i] and [n-i-1]
+            int temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
