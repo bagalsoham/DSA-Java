b6b6877779af8349a873ba0ad9e7d78abb4b2b8c
Soham <bagalsoham1717@gmail.com>
2025-08-06 20:18:19 +0530

    Recursion with backtracking and forward

--- AllIndices.java
+++ AllIndices.java
@@ -1,3 +1,31 @@
 public class AllIndices {
-    
+    public static int[] allIndices(int[] arr, int si, int data, int count) {
+        // Base case
+        if (si == arr.length) {
+            int[] base = new int[count];
+            return base;
+        }
+
+        int[] indices;
+        if (arr[si] == data) {
+            indices = allIndices(arr, si + 1, data, count + 1);
+            indices[count] = si; // Store current index a t correct position
+        } else {
+            indices = allIndices(arr, si + 1, data, count);
+        }
+
+        return indices; // ✅ return final array
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 5, 5, 9, 5};
+        int data = 5;
+
+        int[] result = allIndices(arr, 0, data, 0);
+
+        System.out.print("Indices of " + data + ": ");
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
 }
